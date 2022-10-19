2369bdbb0ba2c5da6b08d54aa3eda9c4bb15582c
Joshua Miller <joshua.ryan.miller@icloud.com>
2022-10-19 01:29:07 -0700

    still working on Challenges Set 4

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,37 +9,49 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            int result = 0;
+            foreach (int n in numbers) if (n%2 == 0){result += n;} else {result -= n;}
+            return result;
         }
-
+       
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            int[] stringLengths = {str1.Length, str2.Length, str3.Length, str4.Length};
+            return stringLengths.Min();
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            int[] numbers = { number1, number2, number3, number4 };
+            return numbers.Min();
         }
 
-        public void ChangeBusinessNameTo_TrueCoders(Business biz)
+        public string ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
+            return biz.Name;
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            int[] numbers = {sideLength1, sideLength2, sideLength3};
+            int sumOfTwoSmallestSides = numbers.Sum() - numbers.Max();
+            if (sumOfTwoSmallestSides <= numbers.Max()) return false;
+            else { return true; }
+
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            bool isNumber = double.TryParse(input, out _);
+            return isNumber;
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            int nullCount = 0;
+            foreach (object element in objs) if (element == null) nullCount = nullCount += 1;
+            return nullCount > objs.Count() ? true : false;
         }
 
         public double AverageEvens(int[] numbers)
